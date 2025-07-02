21100c679fddce3b19cfcd6e005befa4df97e01e
Martin Tesar <tesar.cze@gmail.com>
2025-07-02 09:01:13 +0200

    Update Volume.h

--- src/ios/Volume.h
+++ src/ios/Volume.h
@@ -1,8 +1,13 @@
-#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
 #import <AVFoundation/AVFoundation.h>
 
-#import <Cordova/CDVPlugin.h>
+@interface Volume : CDVPlugin
 
-@interface Volume : CDVPlugin {}
-- (void) getVolume:(CDVInvokedUrlCommand*)command;
+/// Returns the current hardware output-volume (0.0-1.0).
+- (void)getVolume:(CDVInvokedUrlCommand*)command;
+
+/// Registers a persistent JS callback that fires every time the user presses
+/// the volume buttons.
+- (void)setVolumenChangeCallback:(CDVInvokedUrlCommand*)command;
+
 @end
