5cc4c959d524684d3546dafc8334b295fb6c4ff8
Martin Tesar <tesar.cze@gmail.com>
2025-07-02 09:01:45 +0200

    Update Volume.m

--- src/ios/Volume.m
+++ src/ios/Volume.m
@@ -1,48 +1,118 @@
 #import "Volume.h"
 
 @implementation Volume {
-  NSString* changeCallbackId;
+    NSString *changeCallbackId;
 }
 
-- (void) pluginInitialize {
-  [super pluginInitialize];
-  [[AVAudioSession sharedInstance] setActive:YES error:nil];
+#pragma mark - Plugin lifecycle ------------------------------------------------
 
-  [[AVAudioSession sharedInstance] addObserver:self forKeyPath:@"outputVolume" options:NSKeyValueObservingOptionNew context:nil];
+- (void)pluginInitialize
+{
+    [super pluginInitialize];
+
+    AVAudioSession *session = AVAudioSession.sharedInstance;
+
+    // 1) Let our session coexist with the WKWebView/Howler session.
+    [session setCategory:AVAudioSessionCategoryPlayback
+             withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                   error:nil];
+
+    // 2) KVO only works while the session is active.
+    [session setActive:YES error:nil];
+
+    // 3) Start watching the hardware volume.
+    [session addObserver:self
+              forKeyPath:@"outputVolume"
+                 options:NSKeyValueObservingOptionNew
+                 context:nil];
+
+    // 4) Re-wire everything if the system rebuilds the audio stack.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleMediaServicesReset:)
+                                                 name:AVAudioSessionMediaServicesWereResetNotification
+                                               object:session];
 }
 
-- (void) getVolume:(CDVInvokedUrlCommand*)command {
-  [self sendCurrentVolumeTo:command.callbackId];
+- (void)dealloc
+{
+    [self removeObserversAndNotifications];
 }
 
-- (void) setVolumenChangeCallback:(CDVInvokedUrlCommand*) command {
-  if (changeCallbackId) {
-    NSLog(@"Overwritting volumeChangeCallback: %@!", changeCallbackId);
-  }
-
-  changeCallbackId = command.callbackId;
+- (void)pluginReset   // Cordova calls this on page reload
+{
+    [self removeObserversAndNotifications];
 }
+
+#pragma mark - JavaScript API ---------------------------------------------------
+
+- (void)getVolume:(CDVInvokedUrlCommand *)command
+{
+    [self sendCurrentVolumeTo:command.callbackId];
+}
+
+- (void)setVolumenChangeCallback:(CDVInvokedUrlCommand *)command
+{
+    if (changeCallbackId) {
+        NSLog(@"Overwriting volumeChangeCallback: %@!", changeCallbackId);
+    }
+    changeCallbackId = command.callbackId;
+}
+
+#pragma mark - KVO --------------------------------------------------------------
 
 - (void)observeValueForKeyPath:(NSString *)keyPath
                       ofObject:(id)object
-                        change:(NSDictionary *)change
-                       context:(void *)context {
-  if (changeCallbackId && [keyPath isEqual:@"outputVolume"]) {
-    [self sendCurrentVolumeTo:changeCallbackId];
-  }
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                       context:(void *)context
+{
+    if ([keyPath isEqualToString:@"outputVolume"] && changeCallbackId) {
+        [self sendCurrentVolumeTo:changeCallbackId];
+    }
 }
 
-- (void) sendCurrentVolumeTo:(NSString*) callbackId {
-  CDVPluginResult* result = [self currentVolume];
-  [result setKeepCallback:[NSNumber numberWithBool:YES]];
-  [self.commandDelegate sendPluginResult: result callbackId:callbackId];
+#pragma mark - AVAudioSession notifications ------------------------------------
+
+- (void)handleMediaServicesReset:(NSNotification *)notification
+{
+    // The system rebuilt all audio objects. Re-configure and re-observe.
+    AVAudioSession *session = AVAudioSession.sharedInstance;
+
+    [session setCategory:AVAudioSessionCategoryPlayback
+             withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                   error:nil];
+    [session setActive:YES error:nil];
+
+    [session addObserver:self
+              forKeyPath:@"outputVolume"
+                 options:NSKeyValueObservingOptionNew
+                 context:nil];
 }
 
-- (CDVPluginResult*) currentVolume {
-  AVAudioSession* audioSession = [AVAudioSession sharedInstance];
-  float volume = audioSession.outputVolume;
+#pragma mark - Helpers ----------------------------------------------------------
 
-  return [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDouble:volume];
+- (void)sendCurrentVolumeTo:(NSString *)callbackId
+{
+    CDVPluginResult *result = [self currentVolume];
+    result.keepCallback = @YES;          // stay alive for future events
+    [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+}
+
+- (CDVPluginResult *)currentVolume
+{
+    float volume = AVAudioSession.sharedInstance.outputVolume;
+    return [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                              messageAsDouble:volume];
+}
+
+- (void)removeObserversAndNotifications
+{
+    AVAudioSession *session = AVAudioSession.sharedInstance;
+
+    @try {
+        [session removeObserver:self forKeyPath:@"outputVolume"];
+    } @catch (NSException * __unused ex) { }
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
